213a6590f73173c842879da1c491b4c4effe8258
ian damping <iandamping92@gmail.com>
2020-09-10 14:39:06 +0700

    generate random color for chips

--- app/src/main/java/com/junemon/gamesapi/feature/genre/GenreViewHolder.kt
+++ app/src/main/java/com/junemon/gamesapi/feature/genre/GenreViewHolder.kt
@@ -18,9 +18,19 @@ class GenreViewHolder(private val binding: ItemGenreBinding) :
 
     fun bind(data: GamesItem) {
         binding.run {
+            val textLong = data.name.length
+
+            val texts:String = when{
+                textLong >= 14 ->{
+                    data.name.subSequence(0, 14).toString() + "..."
+                }
+                else ->{
+                    data.name
+                }
+            }
             chipItemText.chipBackgroundColor =
                 ColorStateList.valueOf(Color.parseColor(generateRandomHexColor()))
-            chipItemText.text = data.name
+            chipItemText.text = texts
         }
     }
 
